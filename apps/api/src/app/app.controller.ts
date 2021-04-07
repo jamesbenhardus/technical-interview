a0e76274ad30a28f266eea398b44941f81ecd7e0
James Benhardus <james.benhardus@gmail.com>
2021-04-07 15:49:19 -0500

    setup data for frontend

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -6,8 +6,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @Get('folders')
+  getFolders() {
+    return this.appService.getFolders();
   }
 }
--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { folders } from './data/folders';
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
     return { message: 'Welcome to api!' };
+  }
+
+  getFolders() {
+    return folders;
   }
 }
